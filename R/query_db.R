3e8e72513dcb6feae9772f301a391a691e5805c9
Alex Yakubovich <alex.yakubovich@gmail.com>
2015-01-26 15:02:50 -0500

    switch order of config and query arguments

--- R/query_db.R
+++ R/query_db.R
@@ -1,14 +1,14 @@
 #' Open a connection to a SQL database, run the query, and close the connection. 
 #'
-#' @param query A string containing the SQL query
 #' @param config A list containing the parameters of the data base connection (driver, host, port, dbname, user, password)
+#' @param query A string containing the SQL query 
 #' @param vector A boolean indicating whether the query should return a a vector that is the first column of a table 
 #' @return A data frame containing the results of the redshift query (if vector=FALSE) or a vector containing the first column of the results (if vector=TRUE)
 #' @examples
 #' config <- list(driver="mydriver.redshift.amazonaws.com", port="5431", dbname="mydbname", user="kristorffeson", password="mYs3curePa33word!")
 #' query_redshift('SELECT * FROM mytable', config)
 
-query_db <- function(query, config, vector=F) {
+query_db <- function(config, query, vector=F) {
   
   library('RJDBC')
   library('RPostgreSQL')  
